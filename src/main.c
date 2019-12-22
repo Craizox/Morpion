3db8f9eed84d423e93cb78b935ae2db879d0e89f
Florian Thibaud <florian.thibaud@epita.fr>
2019-12-22 17:29:37 +0100

    split the main in multiples files

--- src/main.c
+++ src/main.c
@@ -1,133 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-static int check_line(char **board, char player)
-{
-    for (int i = 0; i < 3; i++)
-    {
-        int win = 0;
-        for (int j = 0; j < 3 && board[i][j] == player; j++)
-        {
-            win++;
-        }
-        if (win == 3)
-            return 1;
-    }
-    return 0;
-}
-
-
-static int check_col(char **board, char player)
-{
-    for (int i = 0; i < 3; i++)
-    {
-        int win = 0;
-        for (int j = 0; j < 3 && board[j][i] == player; j++)
-        {
-            win++;
-        }
-        if (win == 3)
-            return 1;
-    }
-    return 0;
-}
-
-static int check_ldiag(char **board, char player)
-{
-    int win = 0;
-    for (int i = 0, j = 0; i < 3 && j < 3 && board[i][j] == player; j++, i++)
-    {
-        win ++;
-    }
-    if (win == 3)
-        return 1;
-    return 0;
-}
-
-
-static int check_rdiag(char **board, char player)
-{
-    int win = 0;
-    for (int i = 2, j = 0; i >= 0 && j < 3 && board[i][j] == player; j++, i--)
-    {
-        win ++;
-    }
-    if (win == 3)
-        return 1;
-    return 0;
-}
-
-static int check_win(char **board, char player)
-{
-    if (check_line(board, player))
-        return 1;
-    if (check_col(board, player))
-        return 1;
-    if (check_ldiag(board, player))
-        return 1;
-    if (check_rdiag(board, player))
-        return 1;
-    return 0;
-}
-
-
-static void print_board(char **board)
-{
-    printf("    A   B   C\n");
-    for (int i = 0; i < 3; i++)
-    {
-        printf("   -----------\n");
-        for (int j = 0; j < 3; j++)
-        {
-            if (j == 0)
-                printf("%d | %c |", i, board[i][j]);
-            else
-                printf(" %c |", board[i][j]);
-        }
-        printf("\n");
-    }
-    printf("   -----------\n");
-    printf("\n");
-}
-
-static char **board_init(void)
-{
-    char **board = calloc(3, sizeof(char *));
-    for (int i = 0; i < 3; i ++)
-    {
-        board[i] =  calloc(3, sizeof(char));
-        for (int j = 0; j < 3; j++)
-        {
-            board[i][j] = ' '; // 32 white space
-        }
-    }
-    return board;
-}
-
-static int check_full_board(char **board)
-{
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            if (board[i][j] == ' ')
-                return 0;
-        }
-    }
-    return 1;
-}
-
-static int put_char_in_board(char **board, char player, char *input)
-{
-    if (input[0] == 'A' || input[0] == 'a')
-        board[input[1] - '0'][0] = player;
-    else if (input[0] == 'B' || input[0] == 'c')
-        board[input[1] - '0'][1] = player;
-    else
-        board[input[1] - '0'][2] = player;
-    return 1;
-}
+#include "check_win.h"
+#include "board.h"
 
 static int check_valid(char *buf)
 {
@@ -139,7 +14,6 @@ static int check_valid(char *buf)
         return 0;
     return 1;
 }
-
 
 static int ask_for_player(char **board, char player)
 {
@@ -160,8 +34,6 @@ static int ask_for_player(char **board, char player)
 
     return put_char_in_board(board, player, buf);
 }
-
-
 
 
 int main(void)
