12d7b8be37278ab72d31531df7f3f0c5f3118083
Florian Thibaud <florian.thibaud@epita.fr>
2019-12-22 20:27:15 +0100

    Allow to play multiples games

--- src/main.c
+++ src/main.c
@@ -46,33 +46,45 @@ static void ask_player_name(char **p1, char **p2)
 
 int main(void)
 {
-    char **board = board_init();
-    int turn_player = 0;
-    char *player1;
-    char *player2;
-    ask_player_name(&player1, &player2);
-    int win_p1, full, win_p2;
-    while (!(win_p2 = check_win(board, 'x')) && !(win_p1 = check_win(board, 'o')) && !(full = check_full_board(board)))
+    int play = 1;
+    while (play)
     {
-        if (turn_player % 2 == 0)
+        char *player1;
+        int turn_player = 0;
+        char *player2;
+        char **board = board_init();
+        ask_player_name(&player1, &player2);
+        int win_p1 = 0, full = 0, win_p2 = 0;
+        while (!(win_p2 = check_win(board, 'x')) && !(win_p1 = check_win(board, 'o')) && !(full = check_full_board(board)))
         {
-            if (ask_for_player(board, 'o', player1))
-                turn_player++;
-        }
-        else
-        {
-            if (ask_for_player(board, 'x', player2))
-                turn_player++;
-        }
+            if (turn_player % 2 == 0)
+            {
+                if (ask_for_player(board, 'o', player1))
+                    turn_player++;
+            }
+            else
+            {
+                if (ask_for_player(board, 'x', player2))
+                    turn_player++;
+            }
 
+        }
+        print_board(board);
+        if (full)
+            printf("Egality\n");
+        if (win_p1)
+            printf("%s Won\n", player1);
+        if (win_p2)
+            printf("%s Won\n", player2);
+        free_board(board);
+
+        printf("Do you want to play again [Y/n]: ");
+        char *p;
+        scanf("%ms", &p);
+        if (p[0] == 'y' || p[0] == 'Y')
+            play = 1;
+        else
+            play = 0;
     }
-    print_board(board);
-    if (full)
-        printf("Egality\n");
-    if (win_p1)
-        printf("%s Won\n", player1);
-    if (win_p2)
-        printf("%s Won\n", player2);
-    free_board(board);
     return 0;
 }
